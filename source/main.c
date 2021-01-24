3f2341366d6240f9a1afba407d11cea69134a9b8
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2021-01-23 16:21:34 -0800

    Add debug socket

--- source/main.c
+++ source/main.c
@@ -1,3 +1,7 @@
+//#define DEBUG_SOCKET
+#define DEBUG_IP "192.168.2.2"
+#define DEBUG_PORT 9023
+
 #include "ps4.h"
 
 #define SPOOF 0x82
@@ -8,12 +12,22 @@ int _main(struct thread *td) {
   initKernel();
   initLibc();
 
+#ifdef DEBUG_SOCKET
+  initNetwork();
+  DEBUG_SOCK = SckConnect(DEBUG_IP, DEBUG_PORT);
+#endif
+
   jailbreak();
   spoof_target_id(SPOOF);
 
   initSysUtil();
 
   printf_notification("Spoofing Target ID: 0x%hhx!", SPOOF);
+
+#ifdef DEBUG_SOCKET
+  printf_socket("\nClosing socket...\n\n");
+  SckClose(DEBUG_SOCK);
+#endif
 
   return 0;
 }
